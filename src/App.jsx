b1e7c435812296b284c413f92810bba12f553fd9
ParvDev <parvdev0109@gmail.com>
2023-10-05 00:10:33 +0530

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/signin'
+import Signup from './pages/Signup'
 
 function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/about" element={ <About /> } /> 
+      <Route path="/profile" element={ <Profile /> } /> 
+      <Route path="/" element={ <Home /> } /> 
+      <Route path="/signin" element={ <Signin /> } /> 
+      <Route path="/signup" element={ <Signup /> } /> 
+    </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>signup</div>
+  )
+}
+
+export default Signup
