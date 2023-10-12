f25e55ad400373532c3cdaee23c61efbca112acd
ParvDev <parvdev0109@gmail.com>
2023-10-12 07:27:18 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import router from "./Routes/user.route.js";
+import userRouter from "./Routes/user.route.js";
+import authRouter from "./Routes/auth.route.js";
+
 
 dotenv.config();
 
@@ -14,10 +16,12 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("listening on port 3000!!!");
 });
 
 
-app.use('/api/user', router);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
